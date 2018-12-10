1a7fc13598beafdf7d387d578ca66e40346760e4
Jorge Ovalle <jroz9105@gmail.com>
2018-12-10 17:19:36 -0600

    * Remove placeholder `ReplaceMe.swift` * Add files for basic implementation.

---
+++ WLEmptyState/Classes/EmptyStateDataSource.swift
@@ -0,0 +1,37 @@
+//
+//  EmptyStateDataSource.swift
+//  WLEmptyState
+//
+//  Created by Jorge Ovalle on 12/6/18.
+//  Copyright © 2018 Wizeline. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol WLEmptyStateDataSource: class {
+    
+    func imageForEmptyDataSet() -> UIImage?
+    
+    func titleForEmptyDataSet() -> NSAttributedString
+    
+    func descriptionForEmptyDataSet() -> NSAttributedString
+    
+}
+
+extension WLEmptyStateDataSource {
+    
+    func imageForEmptyDataSet() -> UIImage? {
+        return UIImage(named: "Empty")
+    }
+    
+    func titleForEmptyDataSet() -> NSAttributedString {
+        let title = NSAttributedString(string: "Empty DataSet", attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline)])
+        return title
+    }
+    
+    func descriptionForEmptyDataSet() -> NSAttributedString {
+        let title = NSAttributedString(string: "Oops There's no data.", attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
+        return title
+    }
+}
---
+++ WLEmptyState/Classes/EmptyStateDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  EmptyStateDelegate.swift
+//  WLEmptyState
+//
+//  Created by Jorge Ovalle on 12/6/18.
+//  Copyright © 2018 Wizeline. All rights reserved.
+//
+
+import Foundation
+
+protocol WLEmptyStateDelegate: class {
+    //emptyDataSetShouldAllowTouch
+}
+
