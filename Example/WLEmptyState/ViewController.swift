ec6a48771779e3e2a2254d764a24162797de380c
Jorge Ovalle <jroz9105@gmail.com>
2018-12-10 12:45:19 -0600

    Initial commit

---
+++ Example/WLEmptyState/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WLEmptyState
+//
+//  Created by lojals on 12/10/2018.
+//  Copyright (c) 2018 lojals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
