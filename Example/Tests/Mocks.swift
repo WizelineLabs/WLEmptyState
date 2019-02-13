709228c636dbc95d6e49c287540ab6dd1c9093da
Jorge Ovalle <jorge.ovalle@wizeline.com>
2019-02-13 17:50:03 -0600

    Add tests for DataSource methods

---
+++ Example/Tests/Mocks.swift
@@ -0,0 +1,49 @@
+//
+//  Mocks.swift
+//  WLEmptyState_Example
+//
+//  Created by Jorge Ovalle on 2/13/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import WLEmptyState
+
+class DefaultTableViewController: UITableViewController, WLEmptyStateDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.emptyStateDataSource = self
+    }
+    
+}
+
+class CustomTableViewController: UITableViewController, WLEmptyStateDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.emptyStateDataSource = self
+    }
+    
+    func titleForEmptyDataSet() -> NSAttributedString {
+        return NSAttributedString(string: "Test title")
+    }
+    
+    func descriptionForEmptyDataSet() -> NSAttributedString {
+        return NSAttributedString(string: "Test description")
+    }
+    
+    func imageForEmptyDataSet() -> UIImage? {
+        return nil
+    }
+}
+
+enum DefaultDataSource {
+    static let title = NSAttributedString(string: "Empty DataSet",
+                                          attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline)])
+    
+    static let description = NSAttributedString(string: "Oops There's no data.",
+                                                attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
+}
