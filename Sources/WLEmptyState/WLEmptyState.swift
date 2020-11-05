87830d636b5c35a113252ca669b07e8c3173cf39
Jorge Ovalle <jroz9105@gmail.com>
2020-11-04 20:34:24 -0600

    Add first version of SPM support

---
+++ Sources/WLEmptyState/WLEmptyState.swift
@@ -0,0 +1,3 @@
+struct WLEmptyState {
+    var text = "Hello, World!"
+}
---
+++ Tests/WLEmptyStateTests/WLEmptyStateTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import WLEmptyState
+
+final class WLEmptyStateTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WLEmptyState().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
